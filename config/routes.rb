7025d032b8aaa2824147d5ce57752d56ba615e85
Killian Portier <killian.portier@yahoo.fr>
2024-05-24 11:58:24 +0200

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "lists#index"
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:create]
     resources :reviews, only: [:create]
   end
