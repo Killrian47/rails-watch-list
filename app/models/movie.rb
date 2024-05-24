4bd7b206001391b8893cb73f71ff62c36f9ef156
Killian Portier <killian.portier@yahoo.fr>
2024-05-24 16:37:42 +0200

    create comment model link to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movie_tags, dependent: :destroy
   has_many :tags, through: :movie_tags
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
